6ece3f909e7afeadde09579be445612fba0057ba
Raymond Ji <raymondji.me@gmail.com>
2015-11-27 15:16:59 -0500

    Fix email validation to invalidate emails of the form hi@gg..com

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,10 +41,10 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
   	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@g..com]
     invalid_addresses.each do |invalid_address|
     	@user.email = invalid_address
-    	assert_not @user.valid?, "#{invalid_address.inspect} should be valid"
+    	assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
@@ -63,5 +63,12 @@ class UserTest < ActiveSupport::TestCase
   test "password should have minimum length" do
   	@user.password = @user.password_confirmation = "a" * 5
   	assert_not @user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "Foo@ExAMPle.com"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
