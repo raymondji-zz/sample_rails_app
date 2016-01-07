8b8fab16a3845e54108a17c0173af9fd477426e2
Raymond Ji <raymondji.me@gmail.com>
2016-01-07 16:23:52 -0500

    Improve tests to check remembered_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,20 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
+	# -- Class Methods --
 	# Returns the hash digest of the given string
-	def User.digest(string)
+	def self.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 													  BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost);
 	end
 
 	# Returns a random token
-	def User.new_token
+	def self.new_token
 		return SecureRandom.urlsafe_base64
 	end
 
+	# -- Instance Methods --
 	# Remembers a user in the databse for use in persistent sessions
 	def remember
 		self.remember_token = User.new_token
